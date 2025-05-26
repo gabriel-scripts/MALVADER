b326e99faa31b099654977a6d06d68a5ae839e4d
gabrielScripts <gabrielcardosopassos@gmail.com>
2025-05-25 22:54:39 -0300

    fix: testBase

--- test/testBase.py
+++ test/testBase.py
@@ -8,7 +8,7 @@ from dao.config.database import Base
 class TestBase(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.engine = create_engine('mysql+pymysql://root:verysecret@llocalhost:33789/test_db')
+        cls.engine = create_engine('mysql+pymysql://root:verysecret@localhost:33789/test_db')
         cls.Session = sessionmaker(bind=cls.engine)
         Base.metadata.create_all(cls.engine)
 
